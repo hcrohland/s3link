8e521265eb44ad0c14d8a75586eb2cd093ae268c
Christoph Rohland <hcrohland@users.noreply.github.com>
2024-11-06 13:46:05 +0100

    remove stale do_retransmit

--- main/s3link.c
+++ main/s3link.c
@@ -61,37 +61,6 @@ static bool handle_rx(const uint8_t *data, const size_t data_len, void *sock)
     }
     return true;
 }
- 
-/* static void do_retransmit(const int sock)
-{
-    int len;
-    char rx_buffer[128];
-
-    do {
-        len = recv(sock, rx_buffer, sizeof(rx_buffer) - 1, 0);
-        if (len < 0) {
-            ESP_LOGE(TAG, "Error occurred during receiving: errno %d", errno);
-        } else if (len == 0) {
-            ESP_LOGW(TAG, "Connection closed");
-        } else {
-            rx_buffer[len] = 0; // Null-terminate whatever is received and treat it like a string
-            ESP_LOGI(TAG, "Received %d bytes: %s", len, rx_buffer);
-
-            // send() can return less bytes than supplied length.
-            // Walk-around for robust implementation.
-            int to_write = len;
-            while (to_write > 0) {
-                int written = send(sock, rx_buffer + (len - to_write), to_write, 0);
-                if (written < 0) {
-                    ESP_LOGE(TAG, "Error occurred during sending: errno %d", errno);
-                    // Failed to retransmit, giving up
-                    return;
-                }
-                to_write -= written;
-            }
-        }
-    } while (len > 0);
-} */
 
 static void tcp_server_task(void *pvParameters)
 {
